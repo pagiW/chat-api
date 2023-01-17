c71d53edc98a4dfedb07c7a4e1a200948934f900
Juansito_the_dev <tecnosebastube@gmail.com>
2023-01-17 14:21:36 -0500

    test

--- middlewares/ErrorHandler.py
+++ middlewares/ErrorHandler.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 
 class ErrorHandler(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI) -> None:
-        super().__init__(app)
+        super().__init__(app);
     async def dispatch(self, request: Request, call_next) -> Response:
         try:
             return await call_next(request)
